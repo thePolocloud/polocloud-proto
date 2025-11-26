2eab8c12e09091b327ee3e6795a028d7293bc7a5
httpmarco <httpmarco@gmx.de>
2025-11-26 13:31:45 +0100

    Add .gitattributes for Protocol Buffer files and update group_provider.proto for new response types and field renaming

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,8 +23,6 @@ dependencies {
     api("com.google.protobuf:protobuf-java:$protobufVersion")
     api("io.grpc:grpc-stub:$grpcVersion")
     api("io.grpc:grpc-protobuf:$grpcVersion")
-    api("io.grpc:grpc-services:${grpcVersion}")
-    api("io.grpc:grpc-netty-shaded:${grpcVersion}")
 
     // Compile only annotation API
     compileOnly("javax.annotation:javax.annotation-api:1.3.2")
@@ -75,7 +73,7 @@ protobuf {
 }
 
 tasks.shadowJar {
-    relocate("com.google.protobuf", "dev.httpmarco.polocloud.sdk.java.relocated.protobuf")
+    relocate("com.google.protobuf", "dev.httpmarco.polocloud.v1.relocated.protobuf")
 
     archiveBaseName.set("polocloud-proto")
     archiveClassifier.set(null as String?)
